cbb6a37de5a1b198a506ae2bcea6389baa03c5eb
Andres Benites <andres@benites.org>
2020-08-30 22:52:43 -0700

    substrings finished

---
+++ substrings.rb
@@ -0,0 +1,21 @@
+def substrings(*strings, dict)
+  return_hash = {}
+  strings = strings.join.split(" ")
+  p strings
+  dict.each do |word|
+    strings.each do |subst|
+      if subst.downcase.include?(word.downcase)
+        if return_hash.has_key?(word.downcase)
+          return_hash[word] += 1
+        else
+          return_hash[word] = 1
+        end
+      end
+    end
+  end
+  p return_hash
+end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
